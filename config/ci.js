5ed079aaf2ad62548fd02a3e0c9b73aa31e6ba97
El Ghazi Mouad <elghazi.mouad1@gmail.com>
2019-11-04 18:41:51 +0100

    Using Amazon S3

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://mouad:mouadpsw@cluster0-shard-00-00-4zxam.mongodb.net:27017,cluster0-shard-00-01-4zxam.mongodb.net:27017,cluster0-shard-00-02-4zxam.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
